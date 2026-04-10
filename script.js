50a874e37a4d7c35e56ec8c2e3db06b5732397b0
feelinglikeawaffle <sonjunigel2@gmail.com>
2026-04-10 13:59:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,13 @@
 const planets = {
-    mercury: {
-        name: "Mercury",
-        distSun: 35980000,
-        distEarth: 56974100
-    },
-    venus: {
-        name: "Venus",
-        distSun: 67240000,
-        distEarth: 25700000
-    },
-    earth: {
-        name: "Earth",
-        distSun: 92960000,
-        distEarth: 0
-    },
-    mars: {
-        name: "Mars",
-        distSun: 141600000,
-        distEarth: 33900000
-    },
-    jupiter: {
-        name: "Jupiter",
-        distSun: 483800000,
-        distEarth: 365000000
-    },
-    saturn: {
-        name: "Saturn",
-        distSun: 890800000,
-        distEarth: 746000000
-    },
-    uranus: {
-        name: "Uranus",
-        distSun: 1784000000,
-        distEarth: 1600000000
-    },
-    neptune: {
-        name: "Neptune",
-        distSun: 2793000000,
-        distEarth: 2700000000
-    }
+    mercury: { name: "Mercury", distSun: 35980000, distEarth: 56974100 },
+    venus:   { name: "Venus",   distSun: 67240000, distEarth: 25700000 },
+    earth:   { name: "Earth",   distSun: 92960000, distEarth: 0 },
+    mars:    { name: "Mars",    distSun: 141600000, distEarth: 33900000 },
+    jupiter: { name: "Jupiter", distSun: 483800000, distEarth: 365000000 },
+    saturn:  { name: "Saturn",  distSun: 890800000, distEarth: 746000000 },
+    uranus:  { name: "Uranus",  distSun: 1784000000, distEarth: 1600000000 },
+    neptune: { name: "Neptune", distSun: 2793000000, distEarth: 2700000000 },
+    pluto:   { name: "Pluto",   distSun: 3670000000, distEarth: 3100000000 }
 };
 
 const infoBox = document.getElementById("info-box");
